e092b2f33fce49e0344318b6d6b206bd50b55ed4
alexhroom <alex.room@btinternet.com>
2023-11-17 09:23:42 +0000

    added iexp quadrature to pyfilon

--- README.md
+++ README.md
@@ -8,8 +8,12 @@ Implementation of Filon quadrature in Rust (as [filon](https://crates.io/crates/
 The Filon quadrature is a quadrature for highly oscillatory
 integrals, such as $\int_a^b f(x) sin(mx)$ or $\int_a^b f(x) cos(mx)$.
 
-This package implements the Filon quadrature algorithm in Rust as well as 
+This package implements the Filon quadrature algorithm in Rust as well as
 Python via [`PyO3`](https://github.com/PyO3/pyo3) and [`maturin`](https://github.com/PyO3/maturin).
+
+Supports:
+In Rust: quadrature for discretised functions with $sin(mx)$ and $cos(mx)$
+In Python: quadrature for discretised and non-discretised functions with $sin(mx)$, $cos(mx)$ and $exp(imx)$.
 
 This code ports [John Burkardt's implementation of Filon quadrature](https://people.math.sc.edu/Burkardt/cpp_src/filon/filon.html),
 based on Chase and Fosdick's algorithm in the ACM, [available on Netlib as TOMS 353](https://netlib.org/toms/index.html).
--- tests/test_filon.py
+++ tests/test_filon.py
@@ -1,6 +1,6 @@
 """Tests the Filon quadrature algorithm."""
 
-from pyfilon import filon_fun_sin, filon_fun_cos
+from pyfilon import filon_fun_sin, filon_fun_cos, filon_fun_iexp
 
 import numpy as np
 import pytest
@@ -59,4 +59,20 @@ def test_filon_sin_points(points, function, exact):
 def test_filon_cos_points(points, function, exact):
     """Tests the accuracy of filon_fun_cos over varying mesh sizes."""
     actual = filon_fun_cos(function, 0, 2 * np.pi, 10, points)
+    np.testing.assert_allclose(actual, exact, rtol=1e-2, atol=1e-2)
+
+@pytest.mark.parametrize('points', [161, 321, 641])
+@pytest.mark.parametrize(
+    'function, exact',
+    [
+        (identity, (-1j*np.pi / 5)),
+        (quadratic, (np.pi/25) + 1j*((-3 * np.pi - 2 * np.pi**2) / 5)),
+        (log, -0.00844659 - 0.197627j),
+        (piecewise, (-1j*np.pi / 5)),
+        (pi_sin, -0.012907844945847433672 -0.086192145774330529j),
+    ],
+)
+def test_filon_iexp_points(points, function, exact):
+    """Tests the accuracy of filon_fun_cos over varying mesh sizes."""
+    actual = filon_fun_iexp(function, 0, 2 * np.pi, 10, points)
     np.testing.assert_allclose(actual, exact, rtol=1e-2, atol=1e-2)
